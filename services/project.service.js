93dfb29bac8b8ee2376b839cececc78766aff591
Andrew <a.moskalenko@tg.dunice.net>
2023-10-06 15:35:10 +0300

    remove query string from project service

--- services/project.service.js
+++ services/project.service.js
@@ -1,5 +1,6 @@
 import { projectRepository, userProjectRepository } from "../db/postgres";
 import { tagService } from "../services";
+import { projectSummaryQuery } from './queries/project-summary-query'
 
 export const projectService = {
   async createProject(newProject, user) {
@@ -31,33 +32,7 @@ export const projectService = {
   async getProjectSummary(id) {
     const header = await tagService.getTagByProjectId(id);
     const table = await projectRepository.query(
-      'SELECT\
-      jsonb_build_object(\
-        \'id\', document_id,\
-        \'name\', document_name\
-      ) as document,\
-      jsonb_agg(\
-        jsonb_build_object(\
-          \'tag\', jsonb_build_object(\
-            \'id\', tag_id,\
-            \'title\', tag_title\
-          ),\
-          \'count\', paragraps_count\
-        )\
-      ) as tags\
-      FROM (SELECT\
-        documents.id as document_id,\
-        documents.name as document_name,\
-        tags.id AS tag_id,\
-        tags.title AS tag_title,\
-        COUNT(paragraphs.id) as paragraps_count\
-      FROM documents\
-      full outer join "tags" on "tags"."project_id" = "documents"."project_id"\
-      left join "paragraph-tags" on "paragraph-tags"."tag_id" = "tags"."id"\
-      left join "paragraphs" on "paragraphs"."id" = "paragraph-tags"."paragraph_id"\
-      where "documents"."project_id" = $1\
-      group by documents.id, tags.id) t\
-      group by document_id, document_name;',
+      projectSummaryQuery,
       [id],
     );
 
---
+++ services/queries/project-summary-query.js
@@ -0,0 +1,29 @@
+
+export const projectSummaryQuery = 
+'SELECT\
+      jsonb_build_object(\
+        \'id\', document_id,\
+        \'name\', document_name\
+      ) as document,\
+      jsonb_agg(\
+        jsonb_build_object(\
+          \'tag\', jsonb_build_object(\
+            \'id\', tag_id,\
+            \'title\', tag_title\
+          ),\
+          \'count\', paragraps_count\
+        )\
+      ) as tags\
+      FROM (SELECT\
+        documents.id as document_id,\
+        documents.name as document_name,\
+        tags.id AS tag_id,\
+        tags.title AS tag_title,\
+        COUNT(paragraphs.id) as paragraps_count\
+      FROM documents\
+      full outer join "tags" on "tags"."project_id" = "documents"."project_id"\
+      left join "paragraph-tags" on "paragraph-tags"."tag_id" = "tags"."id"\
+      left join "paragraphs" on "paragraphs"."id" = "paragraph-tags"."paragraph_id"\
+      where "documents"."project_id" = $1\
+      group by documents.id, tags.id) t\
+      group by document_id, document_name;';
